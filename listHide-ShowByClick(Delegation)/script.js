6f6619c64ff3c165e964f339b58d0e0280f36f77
an4ger <an4ger@gmail.com>
2014-02-27 17:00:10 +0200

    listHide-Show

---
+++ listHide-ShowByClick(Delegation)/script.js
@@ -0,0 +1,24 @@
+//add span tag, for click after li and hover effect in css
+var liElements = document.getElementsByTagName('li'),
+    span;
+for (var i=0, len = liElements.length; i < len; i++) {
+    span = document.createElement('span');
+
+    //empty span in li
+    liElements[i].insertBefore(span, liElements[i].firstChild);
+
+    //move text node after span in span
+    span.appendChild(span.nextSibling);
+}
+
+// delegation 
+document.getElementById('tree').onclick = function(event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement,
+        node = target.parentNode.getElementsByTagName('ul')[0];
+
+    // no children
+    if (!node) return;
+
+    node.style.display = node.style.display ? '' : 'none';
+};
